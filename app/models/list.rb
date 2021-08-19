120817cfd15719e98485cf832b80e03b7736c1b8
Marco Kjeldsen <marcojmk@tuta.io>
2021-08-19 15:17:11 +0200

    update

---
+++ app/models/list.rb
@@ -0,0 +1,17 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+
+
+
+
+
+  validates :name, uniqueness: true, presence: true
+
+
+
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+user_serialized = URI.open(url).read
+movies = JSON.parse(user_serialized)
+
+p 'destroy all data'
+
+Movie.destroy_all
+p 'Create new data'
+movies['results'].each do |movie|
+  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
+end
+
+
+
+p 'finished'
